63ee5fe871ca961483786cd19b65ad2f5eeba00e
George Yu <gyu13@illinois.edu>
2025-02-13 16:01:06 -0600

    functions.cc

--- src/functions.cc
+++ src/functions.cc
@@ -35,14 +35,14 @@ bool MakeMove(std::vector<std::vector<char>>& board,
 }
 
 char CheckWinner(const std::vector<std::vector<char>>& board) {
-  for (unsigned int i = 0; i < kBoardSize; ++i) {
+  for (unsigned int i = 0; i < 3; ++i) {
     if (board[i][0] == board[i][1] && board[i][1] == board[i][2] &&
         board[i][0] != ' ') {
       return board[0][i];
     }
   }
 
-  for (unsigned int i = 0; i < kBoardSize; ++i) {
+  for (unsigned int i = 0; i < 3; ++i) {
     if (board[0][i] == board[1][i] && board[1][i] == board[2][i] &&
         board[0][i] != ' ') {
       return board[i][0];
